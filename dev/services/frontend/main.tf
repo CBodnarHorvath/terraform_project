7e42c7e46e72c5394e05db4c9a011c2fac0826c8
Charles Bodnar-Horvath <cbodnarhorvath@palo-it.com>
2023-01-11 08:54:05 +0800

    initial terraform commit

---
+++ dev/services/frontend/main.tf
@@ -0,0 +1,35 @@
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
+  type        = number
+  default     = 80
+}
+
+provider "aws" {
+ region = "ap-southeast-1"
+}
+
+resource "aws_instance" "techsecops-exercise" {
+ ami	= "ami-005835d578c62050d"
+ instance_type = "t2.micro"
+ vpc_security_group_ids = [aws_security_group.instance.id]
+
+user_data = templatefile("user-data.sh", {
+    server_port = var.server_port
+  })
+
+ user_data_replace_on_change = true
+
+ tags = {
+  Name = "techsecops-exercise"
+ }
+}
+
+resource "aws_security_group" "instance" {
+  name = "techsecops-exercise-instance"
+  ingress {
+    from_port   = var.server_port
+    to_port     = var.server_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
