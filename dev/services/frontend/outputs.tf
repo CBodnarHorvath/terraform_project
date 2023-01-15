7e26ec483554052b450b7a7c9a9750eb98860122
Charles Bodnar-Horvath <cbodnarhorvath@palo-it.com>
2023-01-16 03:46:35 +0800

    update outputs

--- dev/services/frontend/outputs.tf
+++ dev/services/frontend/outputs.tf
@@ -2,7 +2,7 @@ output "public_ip" {
   value       = aws_instance.techsecops-exercise.public_ip
   description = "The public IP address of the web server"
 }
-output "elastic_ip" {
-  value       = aws_instance.myeip.public_ip
-  description = "The elastic IP address of the web server"
+output "public_ip" {
+  description = "Contains the public IP address"
+  value       = aws_eip.default.public_ip
 }
