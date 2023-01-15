e817064aa25a96f52def69ec2c3f7e730826e081
Charles Bodnar-Horvath <cbodnarhorvath@palo-it.com>
2023-01-16 03:58:44 +0800

    elastic ip fixed

--- dev/services/frontend/outputs.tf
+++ dev/services/frontend/outputs.tf
@@ -2,7 +2,3 @@ output "ec2_ip" {
   value       = aws_instance.techsecops-exercise.public_ip
   description = "The public IP address of the web server"
 }
-output "elastic_ip" {
-  description = "Contains the public IP address"
-  value       = aws_eip.myeip.public_ip
-}
