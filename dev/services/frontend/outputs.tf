3fed7898b61eeed50e173fcc353ec8822fc5b493
Charles Bodnar-Horvath <cbodnarhorvath@palo-it.com>
2023-01-16 03:47:55 +0800

    update outputs

--- dev/services/frontend/outputs.tf
+++ dev/services/frontend/outputs.tf
@@ -1,8 +1,8 @@
-output "public_ip" {
+output "ec2_ip" {
   value       = aws_instance.techsecops-exercise.public_ip
   description = "The public IP address of the web server"
 }
-output "public_ip" {
+output "elastic_ip" {
   description = "Contains the public IP address"
   value       = aws_eip.default.public_ip
 }
